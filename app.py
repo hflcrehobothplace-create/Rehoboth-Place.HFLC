14554f967a170cd6a8fca87d64cd0f29e6d3ab2e
hflcrehobothplace-create <hflc.rehobothplace@gmail.com>
2026-04-01 18:35:14 +0100

    Modify app run settings for production
    Updated app run configuration to use environment port and disabled debug mode.
    Also edited the 10 lines of code from ==RUN APP==.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # ==============================================
 # app.py — Flask Backend for Church Website + Admin Panel
 # ==============================================
-# pip install twilio sendgrid
 # Twilio for SMS
 from twilio.rest import Client
 
@@ -266,9 +265,12 @@ def bible_search():
 # ------------------------------------------
 # RUN APP
 # ------------------------------------------
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
    
  
  
